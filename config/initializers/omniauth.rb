063a8b365ab77aabec0ba1228ecc6628591797a6
Rodrigo <rodrigo19962010@live.com>
2017-04-07 09:54:24 -0300

    Start

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
+
+require 'omniauth-facebook'
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.secrets.omniauth_provider_key, Rails.application.secrets.omniauth_provider_secret
+  provider :facebook, "1916873801874478", "74e3cde26b0bde1ab9cf75cd8ea9993f", :display => 'popup'
 end
